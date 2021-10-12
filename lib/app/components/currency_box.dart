a6e4c72ec01570582b61cdac45e92a8797b328b1
Rodrigo Lourenço <rodrigo.lourenco@midway.com.br>
2021-10-12 14:17:03 -0300

    33 - Criando o controller

--- lib/app/components/currency_box.dart
+++ lib/app/components/currency_box.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 class CurrecyBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 1,
-          child: SizedBox(
-            height: 64,
-            child: DropdownButton(
+    return Container(
+      height: 60,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 1,
+            child: DropdownButtonFormField(
               iconEnabledColor: Colors.amber,
               isExpanded: true,
-              underline: Container(height: 1, color: Colors.amber),
+              decoration: InputDecoration(border: OutlineInputBorder()),
               items: [
                 DropdownMenuItem(
                   child: Text('Real'),
@@ -26,21 +27,18 @@ class CurrecyBox extends StatelessWidget {
               onChanged: (value) {},
             ),
           ),
-        ),
-        SizedBox(width: 10),
-        Expanded(
-          flex: 2,
-          child: TextField(
-            onChanged: (text) {},
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.amber),
+          SizedBox(width: 10),
+          Expanded(
+            flex: 2,
+            child: TextField(
+              onChanged: (text) {},
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
